83b144840ad10240145c73b3c6dbd1c0860a25f8
JamesZhang <james.q.zhang@ericsson.com>
2017-06-13 11:07:22 +0800

    support search function

--- ietf-yang-static-webpage/main.py
+++ ietf-yang-static-webpage/main.py
@@ -4,6 +4,8 @@ import jinja2
 import json
 import re
 
+from HTMLParser import HTMLParser
+
 def render(template_path, context):
     path, filename = os.path.split(template_path)
     return jinja2.Environment(
@@ -53,6 +55,7 @@ def utc_time_from(local_time):
 
 
 def run(json_file, path):
+    output_files = []
     with open(json_file) as data_file:
         data = json.load(data_file)
 
@@ -75,19 +78,56 @@ def run(json_file, path):
                 }
 
         d = render("./template/index.html.template",context)
-        html_file = open(os.path.join(path,'index.html'),'w')
+        index_file = os.path.join(path,'index.html')
+        html_file = open(index_file,'w')
         html_file.write(d)
+        output_files.append(index_file)
 
         for area in data:
             filename = os.path.join(path, '%s.html' %(s[area]))
             d = render("./template/area.html.template",{"area_name":area, "data":data, "short_name":s, "t":utc_time})
             html_file = open(filename,'w')
             html_file.write(d)
+            output_files.append(filename)
             print "write file to %s" %(filename)
+
+    return output_files
+
+def generate_search_info(input_files, folder):
+    from operator import itemgetter
+    l = []
+    for file in input_files:
+        if file.find("index.html") != -1:
+            from bs4 import BeautifulSoup
+            bs = BeautifulSoup(open(file),"html.parser")
+            for item in bs.find_all("td","wg"):
+                l.append({"location":"/index.html#{}".format(item.string),"text":item.string,"title":"{}".format(item.string)})
+        else:
+            from bs4 import BeautifulSoup
+            bs = BeautifulSoup(open(file), "html.parser")
+            for item in bs.find_all("tr","doc"):
+                location = "/%s#%s" %(os.path.split(file)[1], item.find("td","doc_name").string)
+                content = " ".join([i.string for i in item.find_all("td")[1:]])
+                content += " " + item.find("td","doc_name").a['href']
+                title = item.find("td","doc_name").string
+
+                l.append({'location':location, 'text':content, 'title':title})
+
+    with open(os.path.join(folder,"search_index.json"),"w") as s_file:
+        import json
+        s_file.write(json.dumps({"docs":l},indent=4))
+        
+
 
 
 if __name__ == '__main__':
+    output_files = None
+    output_folder = "./"
+
     if (len(sys.argv) > 2):
-        run(sys.argv[1], sys.argv[2])
-    else:
-        run(sys.argv[1],"./")
+        output_folder = sys.argv[2]
+
+    output_files = run(sys.argv[1], output_folder)
+
+    generate_search_info(output_files, output_folder)
+
